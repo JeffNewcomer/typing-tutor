889f6100978136fbaa3744944deb5193bb1113d5
Jeff Newcomer <jrnewk@gmail.com>
2018-06-20 13:01:04 -0700

    Create an Object to represent the application state, and make the characters a property.

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-
-
-var textToType = 'grumpy wizards make toxic brew for the evil queen and jack'
-var lettersToType = []
-
+// creates a div element to hold the Typing Tutor Text
 var $title = document.createElement('div')
 $title.textContent = "Typing Tutor"
 document.body.appendChild($title)
@@ -17,13 +13,31 @@ $title.style.padding = '100px'
 // $container.style.height = '120px'
 // $container.style.border = "2px solid black"
 
+var textToType = 'grumpy wizards make toxic brew for the evil queen and jack'
+var lettersToType = []
+
+// splits the textToType string into individual letters
+var letters = textToType.split('')
+
 // creates an object for each letter in the sentence. each object looks like this {letter: "g"}
 function makeLetter(letter) {
   return {
     letter: letter
   }
 }
-// function that creates the div elements on the page
+
+// loop that creates the array of letters (using the makeLetter function above)
+for (var i = 0; i < letters.length; i++) {
+  lettersToType.push(makeLetter(letters[i]))
+}
+
+
+var appState = {
+  characters: lettersToType
+}
+console.log(appState)
+
+// function that creates the individual div elements on the page for each letter
 function renderLetter(letter) {
   var $letter = document.createElement('div')
   $letter.textContent = letter.letter
@@ -33,15 +47,8 @@ function renderLetter(letter) {
   //document.$container.appendChild($letter)
   return $letter
 }
-// splits the textToType string into individual letters
-var letters = textToType.split('')
 
-// loop that creates the array of letters (using the makeLetter function above)
-for (var i = 0; i < letters.length; i++) {
-  lettersToType.push(makeLetter(letters[i]))
-}
-
-// adds the grumpy letters to the body of the page
+// appends the letters from the sentence to the body of the page
 for (var i = 0; i < lettersToType.length; i++) {
   document.body.appendChild(renderLetter(lettersToType[i]))
 }
