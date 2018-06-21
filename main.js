3e71f4637a3614f1d81dfafeff2eeb978922fd5a
Jeff Newcomer <jrnewk@gmail.com>
2018-06-21 12:44:19 -0700

    Added an event listener to the window to listen for 'keydown' events that re-renders the phrase. #5

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+
+
+
 // creates a div element to hold the Typing Tutor Text
 var $title = document.createElement('div')
 $title.textContent = "Typing Tutor"
@@ -6,12 +10,14 @@ $title.style.fontSize = '50px'
 $title.style.textAlign = 'center'
 $title.style.padding = '100px'
 
-// var $container = document.createElement('div')
-// document.body.appendChild($container)
-// $container.style.textAlign = 'center'
-// $container.style.width = 'auto'
-// $container.style.height = '120px'
-// $container.style.border = "2px solid black"
+// creates a div container to hold the individual div elements that contain each letter in the "grumpy' phrase"
+var $container = document.createElement('div')
+document.body.appendChild($container)
+$container.style.textAlign = 'center'
+$container.style.width = 'auto'
+$container.style.height = '32px'
+$container.style.border = "2px solid black"
+
 
 var textToType = 'grumpy wizards make toxic brew for the evil queen and jack'
 var lettersToType = []
@@ -31,33 +37,43 @@ for (var i = 0; i < letters.length; i++) {
   lettersToType.push(makeLetter(letters[i]))
 }
 
-
 var appState = {
   characters: lettersToType,
   currentCharacter: 0,
   failures: 0
 }
 
-
-
-// function that creates the individual div elements on the page for each letter
+// function that creates the individual div elements with the letters in them on the page
 function renderLetter(letter, currentCharacter, index) {
   var $letter = document.createElement('div')
   $letter.textContent = letter.letter
   $letter.style.cssFloat = 'left';
-  $letter.style.fontSize = '20px';
+  $letter.style.fontSize = '17px';
   $letter.style.padding = '6px';
-  console.log(letter, currentCharacter, index);
+  $letter.style.border = 'solid black 1px'
+
+
+  // changes the current letter's color to white
   if (currentCharacter === index) {
     $letter.classList.add('currentCharacter')
-
-
   }
-  //document.$container.appendChild($letter)
   return $letter
 }
 
-// appends the letters from the sentence to the body of the page
-for (var i = 0; i < lettersToType.length; i++) {
-  document.body.appendChild(renderLetter(lettersToType[i], appState.currentCharacter, i))
+// "renders" all the letters from the sentence to the container
+function renderThePhrase() {
+  for (var i = 0; i < lettersToType.length; i++) {
+    $container.appendChild(renderLetter(lettersToType[i], appState.currentCharacter, i))
+    }
 }
+renderThePhrase()
+
+window.addEventListener('keydown', function(event) {
+  if (event.key === lettersToType[appState.currentCharacter].letter) {
+    appState.currentCharacter = appState.currentCharacter +1;
+    $container.innerHTML = '';
+    renderThePhrase()
+  }
+  console.log('keydown');
+  console.log(event.key);
+})
